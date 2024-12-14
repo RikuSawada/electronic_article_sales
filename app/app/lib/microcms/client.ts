66d43cc78c005d06772c0f1dc4431a8e61686b7e
Riku Sawada <rikusawada@RikunoMacBook-Pro.local>
2024-12-14 18:07:54 +0900

    MicroCSMからの取得にキャッシュを保持しないように修正。

--- app/app/lib/microcms/client.ts
+++ app/app/lib/microcms/client.ts
@@ -8,6 +8,9 @@ export const client = createClient({
 
 export const getAllBooks = async () => {
   const allBooks = await client.get({
+    customRequestInit: {
+      cache:"no-store",
+    },
     endpoint: "e-book",
     queries: {
       offset: 0,
@@ -20,6 +23,9 @@ export const getAllBooks = async () => {
 
 export const getDetailBook = async (contentId: string) => {
   const detailBook = await client.getListDetail<BookType>({
+    customRequestInit : {
+      cache:"no-store",
+    },
     endpoint: "e-book",
     contentId,
   });
